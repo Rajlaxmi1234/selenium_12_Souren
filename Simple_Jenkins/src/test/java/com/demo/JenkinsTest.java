89b6e20c9a05f462567bdb1e61d7c2368d22d056
Rajlaxmi <rajlaxmia23@gmail.com>
2025-02-06 13:55:02 +0530

    pollSCM

--- Simple_Jenkins/src/test/java/com/demo/JenkinsTest.java
+++ Simple_Jenkins/src/test/java/com/demo/JenkinsTest.java
@@ -1,5 +1,9 @@
 package com.demo;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
@@ -7,5 +11,10 @@ public class JenkinsTest {
 @Test
 public void jenkins() {
 	Reporter.log("Jenkins class added", true);
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	driver.get("8https://demowebshop.tricentis.com/");
+	Reporter.log("DWS launched successfully",true);
 }
 }
---
+++ Simple_Jenkins/src/test/java/com/demo/PollSCMTest.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollSCMTest {
+@Test
+public void pollSCM() {
+	Reporter.log("PollSCM class successfully added", true);
+}
+}
