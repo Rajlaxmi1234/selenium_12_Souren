fd34368d37a3a10db74b38b82f07906855eaf0a5
Rajlaxmi <rajlaxmia23@gmail.com>
2025-02-06 13:04:51 +0530

    Jenkins

---
+++ Simple_Jenkins/src/test/java/com/demo/JenkinsTest.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+@Test
+public void jenkins() {
+	Reporter.log("Jenkins class added", true);
+}
+}
